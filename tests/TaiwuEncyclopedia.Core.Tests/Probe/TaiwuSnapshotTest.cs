265ba8661d0da037dbebfb1ca24f594d7434b0b6
alwayssuper <12851801+alwayssuper@user.noreply.gitee.com>
2026-07-25 22:11:52 +0800

    feat(probe): TaiwuSnapshot DTO - taiwu full state

---
+++ tests/TaiwuEncyclopedia.Core.Tests/Probe/TaiwuSnapshotTest.cs
@@ -0,0 +1,22 @@
+using FluentAssertions;
+using TaiwuEncyclopedia.Core.Probe.Dto;
+using Xunit;
+
+namespace TaiwuEncyclopedia.Core.Tests.Probe;
+
+public class TaiwuSnapshotTest
+{
+    [Fact]
+    public void Defaults_AreEmptyOrNull()
+    {
+        var s = new TaiwuSnapshot();
+        s.Name.Should().Be("");
+        s.FiveElementsProportion.Should().HaveCount(5);
+        s.CurMainAttributes.Should().HaveCount(6);
+        s.Resources.Should().HaveCount(8);
+        s.CombatSkillQualifications.Should().HaveCount(14);
+        s.Errors.Should().BeEmpty();
+        s.StanceName.Should().BeNull();
+        s.GenderName.Should().BeNull();
+    }
+}
